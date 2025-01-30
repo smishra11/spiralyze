002b0b8b0338c282bf6dc1a408c0928e9a67b220
Subhasish Mishra <msubhasish11@gmail.com>
2025-01-30 22:53:08 +0530

    fixed styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let currentIndex = 0;
 
 function moveSlide(direction) {
+  const carousel = document.querySelector('.carousel');
   const items = document.querySelectorAll('.carousel-item');
   const totalItems = items.length;
 
@@ -13,7 +14,6 @@ function moveSlide(direction) {
     currentIndex = 0;
   }
 
-  const carousel = document.querySelector('.carousel');
   carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
